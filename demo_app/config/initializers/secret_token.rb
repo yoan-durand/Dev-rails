6b6b29466bcaf82fbe0e7c5a2333c32a0ec7db37
yoan <durand.yoan@gmail.com>
2012-11-22 10:48:58 +0100

    add demo-rails

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '159ab9d182c10c34f2e226d1b16169196cce16428be49001994a7bd772f488c7cee938376e736649cd2443f93d1a78a9a7e95154bb9df9ac34b0dbdce7541962'
--- first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'a63591ad3ab38c7935604d9162bc6b9069c9804204f77b73c7121420992f1292e8c667dbb2b61cc619a0400d8417223c0f388228a4686bce9bd33513cbadb688'
